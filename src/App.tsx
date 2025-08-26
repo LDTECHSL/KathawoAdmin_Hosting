5719b5ce0dab4220ddf71e57ffc394dbebcdb1a1
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2025-08-26 14:48:02 +0530

    refactor: remove unused pages (About, Contact, Dashboard, Feedback, Details, Stat) and clean up App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import Navbar from "./Layouts/Navbar"; // Adjust the path as needed
-import Dashboard from "./Pages/Dashboard";
+import Navbar from "./Layouts/Navbar";
 import Login from "./Pages/LoginPage"; // Import your login page
 import Landing from "./Pages/LandingPage";
-import AboutPage from "./Pages/About";
-import ContactPage from "./Pages/Contact";
-import FeedbackPage from "./Pages/Feedback";
 import Toast from "./Components/Toast";
 
 const App: React.FC = () => {
@@ -20,10 +16,6 @@ const App: React.FC = () => {
         <Navbar>
           <Routes>
             <Route path="/home" element={<Landing />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/feedback" element={<FeedbackPage />} />
             {/* Add more routes as needed */}
           </Routes>
         </Navbar>
